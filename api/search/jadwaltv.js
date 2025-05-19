a6a1669353f52c045e7fc86c8f9434b405e339a8
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-19 13:10:57 +0700

    Add files via upload

---
+++ api/search/jadwaltv.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = {
+    name: 'Jadwal Tv',
+    desc: 'Search info jadwal Tv',
+    category: 'Search',
+    params: ['channel'],
+    async run(req, res) {
+        try {
+            const { channel } = req.query;
+            if (!channel) return res.status(400).json({ status: false, error: 'Query is required' });
+            const url = `https://www.jadwaltv.net/channel/${channel}`;
+            const { data: html } = await axios.get(url);
+            const $ = cheerio.load(html);
+            const result = [];
+            $('tr.jklIv').each((i, el) => {
+              const time = $(el).find('td').first().text().trim();
+              const program = $(el).find('td').last().text().trim();
+              result.push({ time, program });
+            });
+            res.status(200).json({
+                status: true,
+                result: result
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    }
+}
