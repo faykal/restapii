ac0a1047edbfac9bafc3c65973f566fdbac4497b
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-11 18:41:58 +0700

    Update ytmp4

--- api/download/ytmp4.js
+++ api/download/ytmp4.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const { search, ytmp3, ytmp4, ytdlv2, channel } = require('@vreden/youtube_scraper');
 module.exports = {
     name: 'YouTube Video',
     desc: 'Video download',
@@ -8,10 +8,13 @@ module.exports = {
         const { url } = req.query;
         if (!url) return res.status(400).json({ status: false, error: 'Url is required' });
         try {
-            const fay = await axios.get(`https://api.kenshiro.cfd/api/downloader/ytv?url=${url}`)
+            const fay = await ytmp4(url)
             res.status(200).json({
                 status: true,
-                data: fay.data.data
+                data: {
+                    metadata: fay.metadata,
+                    download: fay.download
+                }
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
