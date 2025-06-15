eb9467e4a5b72bcba807c7a22d49af3cb08b6979
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-15 19:38:20 +0700

    Update gempa

--- api/random/gempa.js
+++ api/random/gempa.js
@@ -6,9 +6,23 @@ module.exports = {
     async run(req, res) {
         try {
         const { data } = await axios.get(`https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json`)
+        const faykal = data.Infogempa.gempa
         res.status(200).json({
             status: true,
-            data: data.Infogempa.gempa
+            data: {
+                Tanggal: faykal.Tanggal,
+                Jam: faykal.Jam,
+                DateTime: faykal.DateTime,
+                Coordinates: faykal.Coordinates,
+                Lintang: faykal.Lintang,
+                Bujur: faykal.Bujur,
+                Magnitude: faykal.Magnitude,
+                Kedalaman: faykal.Kedalaman,
+                Wilayah: faykal.Wilayah,
+                Potensi: faykal.Potensi,
+                Dirasakan: faykal.Dirasakan,
+                Shakemap: `https://data.bmkg.go.id/DataMKG/TEWS/${faykal.Shakemap}`
+            }
         });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
